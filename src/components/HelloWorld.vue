c4a99bde4d7695595565808fbf500a1569e63837
elliotloftus <ejloftus@gmail.com>
2019-09-02 13:37:14 -0600

    First commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,49 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+  <div class = "hedge">
+    <h1> Welcome to Hedge </h1>
+  </div>
+    <div class = "btn">
+      <v-btn class="ma-2" outlined color="indigo" v-on:click="getData('https://api.the-odds-api.com/v3/odds/?sport=americanfootball_nfl&region=us&apiKey=7f570b4ff2ebe690715570e96c537c21')">Get betting data</v-btn>
+    </div>
+    <v-layout wrap class = "mt-10">
+      <v-flex xs12 sm4 v-for="game in games" v-if="game.sites.length>0">
+        <div class = "cen"><b>{{ game.teams[1] + ' at ' + game.teams[0]}} </b> </div>
+        {{ }}
+        <div class = "odds">
+          <v-flex v-for="site in game.sites">
+            {{site.site_key + ' ' + site.odds.h2h[0] + ' ' + site.odds.h2h[1]}}
+          </v-flex>
+        </div>
+      </v-flex>
+    </v-layout>
+  </div>
   </div>
 </template>
 
 <script>
+import { METHODS } from 'http';
 export default {
   name: 'HelloWorld',
+  data() {
+    return {
+      games: [],
+    }
+  },
   props: {
     msg: String
+  },
+  methods: {
+    async getData(url) {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        this.games =json.data;
+        console.log(this.games);
+      } catch (error) {
+        console.log(error);
+      }
+    }
   }
 }
 </script>
@@ -54,5 +63,25 @@ li {
 }
 a {
   color: #42b983;
+}
+.btn {
+  text-align: center;
+  margin-top: 10px;
+}
+.hedge {
+  text-align: center;
+  margin-top: 10px;
+}
+
+.heros {
+  margin-top: 20px;
+}
+.cen {
+  margin-top: 20px;
+  text-align: center;
+}
+.odds {
+  margin-left: 20px;
+  margin-top: 20px;
 }
 </style>
