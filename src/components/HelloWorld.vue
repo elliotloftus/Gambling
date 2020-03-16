bd2efc5f30855f8984e8b28f1cd2bf92ed6c5d9a
elliot.loftus <ejloftus@gmail.com>
2020-03-15 18:42:41 -0600

    use dynamic grid

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,20 +4,48 @@
     <h1> Welcome to Hedge </h1>
   </div>
     <div class = "btn">
-      <v-btn class="ma-2" outlined color="indigo" v-on:click="getData('https://api.the-odds-api.com/v3/odds/?sport=americanfootball_nfl&region=us&apiKey=7f570b4ff2ebe690715570e96c537c21')">Get betting data</v-btn>
+      <v-btn class="ma-2" outlined color="indigo" v-on:click="getGames('https://api.the-odds-api.com/v3/odds/?apiKey=7f570b4ff2ebe690715570e96c537c21&sport=upcoming&region=uk')">Get betting data</v-btn>
     </div>
-    <v-layout wrap class = "mt-10">
-      <v-flex xs12 sm4 v-for="game in games" v-if="game.sites.length>0">
-        <div class = "cen"><b>{{ game.teams[1] + ' at ' + game.teams[0]}} </b> </div>
-        {{ }}
-        <div class = "odds">
-          <v-flex v-for="site in game.sites">
-            {{site.site_key + ' ' + site.odds.h2h[0] + ' ' + site.odds.h2h[1]}}
-          </v-flex>
-        </div>
-      </v-flex>
-    </v-layout>
-  </div>
+    <v-container fluid>
+      <v-data-iterator
+        :items="games"
+        hide-default-footer
+      >
+
+      <template v-slot:default="props">
+          <v-row>
+            <v-col
+              v-for="item in props.items"
+              :key="item.sports_nice"
+              cols="12"
+              sm="6"
+              md="4"
+              lg="3"
+            >
+              <v-card>
+                <v-list class="subheading font-weight-bold">
+                  <v-list-item>
+                    <v-list-item-content>{{item.sport_nice}}</v-list-item-content>
+                    <v-list-item-content class="center">{{ item.teams[0] }}</v-list-item-content>
+                    <v-list-item-content class="align-end">{{ item.teams[1] }}</v-list-item-content>
+                  </v-list-item>
+                </v-list>
+
+                <v-divider></v-divider>
+
+                  <v-list dense>
+                    <v-list-item v-for="site in item.sites">
+                      <v-list-item-content>{{site.site_nice}}</v-list-item-content>
+                      <v-list-item-content class="center">{{ site.odds.h2h[0] }}</v-list-item-content>
+                      <v-list-item-content class="align-end">{{ site.odds.h2h[1] }}</v-list-item-content>
+                    </v-list-item>
+                </v-list>
+              </v-card>
+            </v-col>
+          </v-row>
+        </template>
+      </v-data-iterator>
+    </v-container>
   </div>
 </template>
 
@@ -34,7 +62,7 @@ export default {
     msg: String
   },
   methods: {
-    async getData(url) {
+    async getGames(url) {
       try {
         const response = await fetch(url);
         const json = await response.json();
