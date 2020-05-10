6ff545e79033aa68f46a894452e0cbe2d386cead
elliot.loftus <ejloftus@gmail.com>
2020-05-10 13:23:08 -0600

    put in odds

---
+++ src/components/BettingOdds.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class = "oddsList">
+        <h1> Betting Odds </h1>
+        <br>
+        <v-data-table
+            :headers = "headers"
+            :items = "contestents"
+            dark
+            hide-default-footer="true">
+        </v-data-table>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            headers: [
+                {text: 'Contestents', value: `name`},
+                {text: 'Money Line', value: 'plusMinus'},
+                {text: 'Over/Under # of McDobules', value: 'OverUnder'}
+            ],
+            contestents: [
+                {name: 'Nate', plusMinus: '-110', OverUnder: '12'},
+                {name: 'Frankie', plusMinus: '+140', OverUnder: '10'}
+            ]
+        }
+    },    
+}
+</script>
+
+<style scoped>
+.oddsList {
+    width: 50%;
+    margin-left: auto;
+    margin-right:auto
+
+}
+</style>
--- src/components/Coronavores.vue
+++ src/components/Coronavores.vue
@@ -1,25 +1,30 @@
 <template>
     <div class = "coronavores">
-      <img src= '@/assets/Hedge.jpg'>
-      <br>
-      <br>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/e9dWys05_Gw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <br>
+        <h1> All Time Promo Video </h1>
+        <br>
+        <iframe width="700" height="425" src="https://www.youtube.com/embed/e9dWys05_Gw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <br><br><br>
+      <BettingOdds />
     </div>
 </template>
 
 <script>
+import BettingOdds from '@/components/BettingOdds'
 export default {
     data() {
         return {
         
         }
     },
+    components: {BettingOdds}
 }
 </script>
 
 <style scoped>
 .coronavores {
-    text-align: center
+    text-align: center;
+    background-color: #85bb65!important;
 }
 </style>
 
