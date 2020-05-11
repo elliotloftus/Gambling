9d373e97f1171e8e279fee5cdf24bbbca1ac14ee
elliot.loftus <ejloftus@gmail.com>
2020-05-10 18:05:03 -0600

    add bet calculator

--- src/components/BettingOdds.vue
+++ src/components/BettingOdds.vue
@@ -10,15 +10,83 @@
             :headers = "oddsHeaders"
             :items = "contestents"
             dark
-            hide-default-footer="true">
+            hide-default-footer>
         </v-data-table>
         <br><br>
+    <v-card dark>
+    <v-card-title>
+        <span class="centered">GET GAMBLING</span>
+        </v-card-title>
+        <v-card-text>
+        <v-form>
+            <v-container>
+            <v-row>
+                <v-col
+                cols="12"
+                md="3"
+                >
+                <v-text-field
+                    v-model="name"
+                    label="Your Name"
+                    required
+                ></v-text-field>
+                </v-col>
+
+                <v-col
+                cols="12"
+                md="3"
+                >
+                <v-text-field
+                    v-model.number=selectedWager
+                    label="Wager"
+                    :rules="wagerRules"
+                    required
+                ></v-text-field>
+                </v-col>
+
+                <v-col
+                cols="12"
+                md="3"
+                >
+                <v-select
+                    v-model="selectedContestent"
+                    :items="contesters"
+                    label="Contestent"
+                    required
+                ></v-select>
+                </v-col>
+
+                <v-col
+                cols="12"
+                md="3"
+                >
+                <v-select
+                    v-model="selectedBet"
+                    :items="possibleBets"
+                    label = "Bet Options"
+                    required
+                ></v-select>
+                </v-col>
+            </v-row>
+            </v-container>
+        </v-form>
+        </v-card-text>
+        <v-card-actions>
+            <span class = "centered"> Potential Winnings : {{totalWinnings}}  </span>
+            <v-btn class="blue--text darken-1" v-on:click="submitBlog">Submit</v-btn>
+            <v-btn class="blue--text darken-1" v-on:click="dialog = false">Cancel</v-btn>
+            </span>
+            <br>
+        </v-card-actions>
+        </v-card>
+        <br>
+        <br>
         <h1> Betting Feed </h1>
         <v-data-table
             :headers = "feedHeaders"
             :items = "bettingFeed"
             dark
-            hide-default-footer="true">
+            hide-default-footer>
         </v-data-table>
     </div>
     <v-footer class = "social">
@@ -59,9 +127,62 @@ export default {
                 {name: 'Craven Moorehead', amount: 100, betType: '-150', contestant: 'Nate', winnings: 150},
                 {name: 'Jack Oliver Body', amount: 100, betType: 'Under', contestant: 'Frankie', winnings: 200},
                 {name: 'Moe Lester', amount: 100, betType: 'Over', contestant: 'Nate', winnings: 200}
-            ]
+            ],
+            contesters: [
+                'Nate',
+                'Frankie'
+            ],
+            selectedBet: null,
+            selectedContestent:null,
+            possibleBets: ["Must select wager and contestent first"],
+            name:null,
+            wagerRules:[
+                bet => bet > 5 || 'Wager must be be greater than 5 bucks you pussy',
+                bet => Number.isInteger(bet) || 'Wager must be an integer (just round up pussy)'
+            ],
+            selectedWager:null,
+            totalWinnings:null
         }
-    },    
+    },
+    methods: {
+        submitBlog() {
+        
+        },
+        calculateWinnings(betSelected,wagerSelected) {
+            if (betSelected.startsWith('Over') || betSelected.startsWith('Under')) {
+                this.totalWinnings = wagerSelected * 2
+            }
+            else if (betSelected.startsWith('+')) {
+                //must be 4 letter string example +120
+                const odds = betSelected.substring(1,4)
+                console.log("odds are " + odds)
+                this.totalWinnings = Math.floor((wagerSelected * (odds/100))) + wagerSelected
+            }
+            else if (betSelected.startsWith('-')) {
+                //must be 4 letter string example +120
+                const odds = betSelected.substring(1,4)
+                this.totalWinnings = Math.floor((wagerSelected / (odds/100))) + wagerSelected
+            }
+        }
+    },
+    watch: {
+        selectedContestent: function(thisContestent) {
+            for (var item of this.contestents) {
+                if (item.name == thisContestent) {
+                    this.possibleBets = [item.plusMinus, "Over " + item.OverUnder, "Under " + item.OverUnder]
+                }
+            }
+        },
+        selectedBet: function(thisBet) {
+            console.log(thisBet)
+            this.calculateWinnings(thisBet,this.selectedWager)
+        },
+        selectedWager: function(thisWager) {
+            if (this.selectedBet != null) {
+                this.calculateWinnings(this.selectedBet,thisWager)
+            }
+        }
+    },
 }
 </script>
 
@@ -94,4 +215,9 @@ export default {
     margin-top: 10px;
     background-color: #85bb65!important;
   }
+.centered {
+    width: 50%;
+    margin-left: auto;
+    margin-right:auto;
+}
 </style>
